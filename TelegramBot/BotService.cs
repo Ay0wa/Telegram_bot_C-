e85ad48ed8b34ea104d26d772ef9aeb04beb237e
Ay0wa <Ay0wa>
2024-05-24 03:44:10 +0300

    a

--- TelegramBot/BotService.cs
+++ TelegramBot/BotService.cs
@@ -2,12 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using System.Collections.Concurrent;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.Passport;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TeacherReviewBot
@@ -16,8 +19,7 @@ namespace TeacherReviewBot
     {
         private readonly TelegramBotClient _botClient;
         private readonly DatabaseService _databaseService;
-
-        private static ReceiverOptions _receiverOptions;
+        private readonly ConcurrentDictionary<long, int> _pendingReviews = new ConcurrentDictionary<long, int>();
         public BotService(TelegramBotClient botClient)
         {
             _botClient = botClient;
@@ -48,50 +50,21 @@ namespace TeacherReviewBot
 
                 if (messageText == "/start")
                 {
-                    await _botClient.SendTextMessageAsync(chatId, "Добро пожаловать! Вы можете:\n1. Получить список преподавателей\n2. Оставить отзыв");
+                    StartCommand(chatId);
                 }
                 else if (messageText == "получить список преподавателей")
                 {
                     await SendTeachersListAsync(chatId);
                 }
-                else if (messageText.StartsWith("преподаватель"))
-                {
-                    var teacherName = messageText.Substring(13).Trim();
-                    var teacher = _databaseService.GetTeacherByName(teacherName);
-                    if (teacher != null)
-                    {
-                        var reviews = _databaseService.GetReviewsByTeacher(teacher.Id);
-                        var reply = $"{teacher.Name}\nОтзывы:\n" + string.Join("\n", reviews.Select(r => r.Text));
-                        await _botClient.SendTextMessageAsync(chatId, reply);
-                    }
-                    else
-                    {
-                        await _botClient.SendTextMessageAsync(chatId, "Преподаватель не найден");
-                    }
+                else if (messageText == "добавить преподавателя"){
+                    await _botClient.SendTextMessageAsync(chatId, "Чтобы добавить преподавателя, напишите /add ФИО номер телефона почта");
                 }
-                else if (messageText.StartsWith("оставить отзыв"))
+                else if (messageText.StartsWith("/add")){
+                    AddNewTeacher(chatId, messageText);
+                }
+                else if (_pendingReviews.TryGetValue(chatId, out int teacherId))
                 {
-                    var parts = messageText.Split(new[] { ' ' }, 5);
-                    if (parts.Length == 5)
-                    {
-                        var teacherName = parts[2] + ' ' + parts[3];
-                        Console.WriteLine(teacherName);
-                        var reviewText = parts[4];
-                        var teacher = _databaseService.GetTeacherByName(teacherName);
-                        if (teacher != null)
-                        {
-                            _databaseService.AddReview(teacher.Id, reviewText);
-                            await _botClient.SendTextMessageAsync(chatId, "Отзыв добавлен");
-                        }
-                        else
-                        {
-                            await _botClient.SendTextMessageAsync(chatId, "Преподаватель не найден");
-                        }
-                    }
-                    else
-                    {
-                        await _botClient.SendTextMessageAsync(chatId, "Неправильный формат команды. Используйте: оставить отзыв [Имя преподавателя] [Текст отзыва]");
-                    }
+                    await HandleReceivedReview(chatId, teacherId, messageText);
                 }
                 else
                 {
@@ -103,25 +76,74 @@ namespace TeacherReviewBot
                 await HandleCallbackQueryAsync(update.CallbackQuery);
             }
         }
+
+        private async Task StartCommand(long chatId)
+        {
+            var reply = new ReplyKeyboardMarkup(
+                new[]
+                {
+                    new KeyboardButton("Получить список преподавателей"),
+                    new KeyboardButton("Добавить преподавателя")
+                })
+            {
+                ResizeKeyboard = true,
+            };
+            
+            await _botClient.SendTextMessageAsync(chatId, "Добро пожаловать! Вы можете:\n1. Получить список преподавателей\n2. Оставить отзыв\n3. Добавить преподавателя", replyMarkup: reply);
+        }
+
         private async Task SendTeachersListAsync(long chatId)
         {
             var teachers = _databaseService.GetTeachers();
             var inlineKeyboard = new InlineKeyboardMarkup(teachers.Select(t =>
-                new[] { InlineKeyboardButton.WithCallbackData(t.Name, t.Name) }));
+                new[] { InlineKeyboardButton.WithCallbackData(t.Name + ' ' + t.Surname, t.Name + ' ' + t.Surname) }));
 
             await _botClient.SendTextMessageAsync(chatId, "Выберите преподавателя:", replyMarkup: inlineKeyboard);
+        }
+
+        private async Task AddNewTeacher(long chatId, string message){
+            var data = message.Split(new[] { ' ' }, 6);
+            var teacher = new Teacher(data[2], data[1], data[3], data[4], data[5], "a");
+            Console.WriteLine(data[0] + '\n');
+            Console.WriteLine(data[1] + '\n');
+            Console.WriteLine(data[2] + '\n');
+            Console.WriteLine(data[3] + '\n');
+            Console.WriteLine(data[4] + '\n');
+            Console.WriteLine(data[5] + '\n');
+            _databaseService.AddTeacher(teacher);
+            await _botClient.SendTextMessageAsync(chatId, "Преподаватель успешно добавлен!");
         }
         private async Task HandleCallbackQueryAsync(CallbackQuery callbackQuery)
         {
             var chatId = callbackQuery.Message.Chat.Id;
-            var teacherName = callbackQuery.Data;
-            var teacher = _databaseService.GetTeacherByName(teacherName);
-            var reviews = _databaseService.GetReviewsByTeacher(teacher.Id);
-            var reply = $"{teacher.Name}\nОтзывы:\n" + string.Join("\n", reviews.Select(r => r.Text));
-            await _botClient.SendTextMessageAsync(chatId, reply);
+            if(callbackQuery.Data.StartsWith("leave_review")){
+                int teacherId = int.Parse(callbackQuery.Data.Split('_')[2]);
+                await HandleLeaveReview(callbackQuery.Message.Chat.Id, teacherId);
+            }
+            else{
+                var teacherName = callbackQuery.Data;
+                var teacher = _databaseService.GetTeacherByName(teacherName);
+                var reviews = _databaseService.GetReviewsByTeacher(teacher.Id);
+                var reply = $"{teacher.Name}\nОтзывы:\n" + string.Join("\n", reviews.Select(r => r.Text));
+                var inlineKeyboard = new InlineKeyboardMarkup(
+                new[] { InlineKeyboardButton.WithCallbackData("Оставить отзыв", $"leave_review_{teacher.Id}") });
+                await _botClient.SendTextMessageAsync(chatId, reply, replyMarkup: inlineKeyboard);
+                await _botClient.AnswerCallbackQueryAsync(callbackQuery.Id);
+            }
             
+        }
+         private async Task HandleLeaveReview(long chatId, int teacherId)
+        {
+            _pendingReviews[chatId] = teacherId;
+            await _botClient.SendTextMessageAsync(chatId, $"Пожалуйста, напишите ваш отзыв");
+        }
 
-            await _botClient.AnswerCallbackQueryAsync(callbackQuery.Id);
+        private async Task HandleReceivedReview(long chatId, int teacherId, string reviewText)
+        {
+            _databaseService.AddReview(teacherId, reviewText);
+            _pendingReviews.TryRemove(chatId, out _);
+
+            await _botClient.SendTextMessageAsync(chatId, "Спасибо за ваш отзыв!");
         }
         
         private Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, System.Threading.CancellationToken cancellationToken)
@@ -129,5 +151,6 @@ namespace TeacherReviewBot
             Console.WriteLine($"HandleErrorAsync:\n{exception}");
             return Task.CompletedTask;
         }
+
     }
 }
--- TelegramBot/DatabaseService.cs
+++ TelegramBot/DatabaseService.cs
@@ -1,89 +1,124 @@
 using System;
 using System.Collections.Generic;
-using System.Data.SQLite;
+using MySql.Data.MySqlClient;
 
 namespace TeacherReviewBot
 {
     public class DatabaseService
     {
-        private const string ConnectionString = "Data Source=teachers.db";
+        private const string ConnectionString = "Server=127.0.0.1;Database=RateTeacherBot;User ID=root;Password=#;";
 
         public DatabaseService()
         {
             InitializeDatabase();
-            SeedData(); // Добавим начальные данные
+            SeedData();
         }
 
         private void InitializeDatabase()
         {
-            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var connection = new MySqlConnection(ConnectionString))
             {
                 connection.Open();
-                string createTeachersTable = @"CREATE TABLE IF NOT EXISTS Teachers (
-                                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                Name TEXT NOT NULL)";
-                var command = new SQLiteCommand(createTeachersTable, connection);
-                command.ExecuteNonQuery();
 
-                string createReviewsTable = @"CREATE TABLE IF NOT EXISTS Reviews (
-                                                Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                                TeacherId INTEGER NOT NULL,
-                                                Text TEXT NOT NULL,
-                                                FOREIGN KEY(TeacherId) REFERENCES Teachers(Id))";
-                command = new SQLiteCommand(createReviewsTable, connection);
-                command.ExecuteNonQuery();
+                string createTablesSql = @"
+                SET SQL_MODE = 'NO_AUTO_VALUE_ON_ZERO';
+                START TRANSACTION;
+                SET time_zone = '+00:00';
+
+                CREATE TABLE IF NOT EXISTS `added_prof` (
+                    `id_add` INT(11) NOT NULL AUTO_INCREMENT,
+                    `add_name` VARCHAR(128) NOT NULL,
+                    `add_surname` VARCHAR(128) NOT NULL,
+                    `add_patronymic` VARCHAR(128) NOT NULL,
+                    `add_phone` VARCHAR(128) NOT NULL,
+                    `add_email` VARCHAR(128) NOT NULL,
+                    `add_img` VARCHAR(128) NOT NULL,
+                    `id_auth` INT(11) NOT NULL,
+                    PRIMARY KEY (`id_add`),
+                    KEY `R_5` (`id_auth`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+                CREATE TABLE IF NOT EXISTS `admin` (
+                    `id_admin` INT(11) NOT NULL AUTO_INCREMENT,
+                    `admin_login` VARCHAR(128) NOT NULL,
+                    `admin_password` VARCHAR(128) NOT NULL,
+                    PRIMARY KEY (`id_admin`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+                CREATE TABLE IF NOT EXISTS `authentication` (
+                    `id_auth` INT(11) NOT NULL AUTO_INCREMENT,
+                    `auth_name` CHAR(18) DEFAULT NULL,
+                    `auth_time` CHAR(18) DEFAULT NULL,
+                    PRIMARY KEY (`id_auth`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+                CREATE TABLE IF NOT EXISTS `professor` (
+                    `id_prof` INT(11) NOT NULL AUTO_INCREMENT,
+                    `prof_name` VARCHAR(128) NOT NULL,
+                    `prof_surname` VARCHAR(128) NOT NULL,
+                    `prof_patronymic` VARCHAR(128) NOT NULL,
+                    `prof_phone` VARCHAR(128) NOT NULL,
+                    `prof_email` VARCHAR(128) NOT NULL,
+                    `prof_img` VARCHAR(128) NOT NULL,
+                    PRIMARY KEY (`id_prof`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+                CREATE TABLE IF NOT EXISTS `review` (
+                    `id_review` CHAR(18) NOT NULL,
+                    `rev_content` TEXT NOT NULL,
+                    `id_admin` INT(11) NOT NULL,
+                    `id_auth` INT(11) NOT NULL,
+                    `id_prof` INT(11) NOT NULL,
+                    `rev_rating` INT(11) NOT NULL,
+                    PRIMARY KEY (`id_review`),
+                    KEY `R_2` (`id_admin`),
+                    KEY `R_3` (`id_auth`),
+                    KEY `R_4` (`id_prof`)
+                ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+                COMMIT;";
             }
         }
 
         private void SeedData()
         {
-            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var connection = new MySqlConnection(ConnectionString))
             {
                 connection.Open();
-                var command = new SQLiteCommand("DELETE FROM Teachers; DELETE FROM Reviews;", connection);
-                command.ExecuteNonQuery();
-                // Проверка, если данные уже существуют
-                var checkCommand = new SQLiteCommand("SELECT COUNT(*) FROM Teachers", connection);
-                long count = (long)checkCommand.ExecuteScalar();
 
-                if (count == 0) // Если данных нет, добавляем начальные данные
+                var checkExistenceCommand = new MySqlCommand("SELECT COUNT(*) FROM professor", connection);
+                var count = Convert.ToInt32(checkExistenceCommand.ExecuteScalar());
+                if (count > 0)
                 {
-                    var insertCommand = new SQLiteCommand("INSERT INTO Teachers (Name) VALUES (@name)", connection);
-
-                    var teacherNames = new List<string>
-                    {
-                        "Бородачева Л.В.",
-                        "Меньшикова Н.П.",
-                        "Лещенкова Е.О.",
-                        "Норин В.П",
-                        "Рудяк Ю.В."
-                    };
-
-                    foreach (var name in teacherNames)
-                    {
-                        insertCommand.Parameters.Clear();
-                        insertCommand.Parameters.AddWithValue("@name", name);
-                        insertCommand.ExecuteNonQuery();
-                    }
+                    // Данные уже существуют, не будем добавлять снова
+                    return;
                 }
+
+                var command = new MySqlCommand(@"INSERT INTO professor (prof_name, prof_surname, prof_patronymic, prof_phone, prof_email, prof_img) VALUES
+                ('John', 'Doe', 'A.', '1234567890', 'john.doe@example.com', 'aaa'),
+                ('Jane', 'Smith', 'B.', '0987654321', 'jane.smith@example.com', 'aaa')", connection);
+                command.ExecuteNonQuery();
             }
         }
 
+        // Остальные методы для работы с данными
         public List<Teacher> GetTeachers()
         {
             var teachers = new List<Teacher>();
-            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var connection = new MySqlConnection(ConnectionString))
             {
                 connection.Open();
-                var command = new SQLiteCommand("SELECT * FROM Teachers", connection);
+                var command = new MySqlCommand("SELECT * FROM professor", connection);
                 using (var reader = command.ExecuteReader())
                 {
                     while (reader.Read())
                     {
-                        teachers.Add(new Teacher
+                        
+                        teachers.Add(new Teacher(reader.GetString("prof_name"), reader.GetString("prof_surname"),
+                            reader.GetString("prof_patronymic"),reader.GetString("prof_email"),
+                            reader.GetString("prof_phone"), reader.GetString("prof_img"))
                         {
-                            Id = reader.GetInt32(0),
-                            Name = reader.GetString(1)
+                            Id = reader.GetInt32("id_prof"),
                         });
                     }
                 }
@@ -93,21 +128,20 @@ namespace TeacherReviewBot
 
         public Teacher GetTeacherByName(string name)
         {
-            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var connection = new MySqlConnection(ConnectionString))
             {
                 connection.Open();
-                var command = new SQLiteCommand("SELECT * FROM Teachers WHERE Name = @name", connection);
+                var command = new MySqlCommand("SELECT * FROM professor WHERE CONCAT(prof_name, ' ', prof_surname) = @name", connection);
                 command.Parameters.AddWithValue("@name", name);
-                Console.WriteLine(name);
-                Console.WriteLine(command);
                 using (var reader = command.ExecuteReader())
                 {
                     if (reader.Read())
                     {
-                        return new Teacher
+                        return new Teacher(reader.GetString("prof_name"), reader.GetString("prof_surname"),
+                            reader.GetString("prof_patronymic"),reader.GetString("prof_email"),
+                            reader.GetString("prof_phone"), reader.GetString("prof_img"))
                         {
-                            Id = reader.GetInt32(0),
-                            Name = reader.GetString(1)
+                            Id = reader.GetInt32("id_prof"),
                         };
                     }
                 }
@@ -118,10 +152,10 @@ namespace TeacherReviewBot
         public List<Review> GetReviewsByTeacher(int teacherId)
         {
             var reviews = new List<Review>();
-            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var connection = new MySqlConnection(ConnectionString))
             {
                 connection.Open();
-                var command = new SQLiteCommand("SELECT * FROM Reviews WHERE TeacherId = @teacherId", connection);
+                var command = new MySqlCommand("SELECT * FROM review WHERE id_prof = @teacherId", connection);
                 command.Parameters.AddWithValue("@teacherId", teacherId);
                 using (var reader = command.ExecuteReader())
                 {
@@ -129,63 +163,78 @@ namespace TeacherReviewBot
                     {
                         reviews.Add(new Review
                         {
-                            Id = reader.GetInt32(0),
-                            TeacherId = reader.GetInt32(1),
-                            Text = reader.GetString(2)
+                            Id = reader.GetString("id_review"),
+                            TeacherId = reader.GetInt32("id_prof"),
+                            Text = reader.GetString("rev_content"),
+                            
                         });
                     }
                 }
             }
             return reviews;
         }
-        public double GetAverageRateByTeacher(int teacherId)
+        public void AddTeacher(Teacher teacher)
         {
-            double SumRates = 0;
-            double CountRates = 0;
-            using (var connection = new SQLiteConnection(ConnectionString))
+            using (var connection = new MySqlConnection(ConnectionString))
             {
                 connection.Open();
-                var command = new SQLiteCommand("SELECT * FROM Reviews WHERE TeacherId = @teacherId", connection);
-                command.Parameters.AddWithValue("@teacherId", teacherId);
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        SumRates += reader.GetInt32(3);
-                        CountRates += 1;
-                    }
-                }
-            }
-            return SumRates/CountRates;
-        }
 
-        
+                var command = new MySqlCommand(@"INSERT INTO professor (prof_name, prof_surname, prof_patronymic, prof_phone, prof_email, prof_img) VALUES
+                (@name, @surname, @patronymic, @phonenumber, @email, @img)", connection);
+                command.Parameters.AddWithValue("@name", teacher.Name);
+                command.Parameters.AddWithValue("@surname", teacher.Surname);
+                command.Parameters.AddWithValue("@patronymic", teacher.Patronymic);
+                command.Parameters.AddWithValue("@phonenumber", teacher.PhoneNumber);
+                command.Parameters.AddWithValue("@email", teacher.Email);
+                command.Parameters.AddWithValue("@img", teacher.Image);
 
-        public void AddReview(int teacherId, string text)
-        {
-            using (var connection = new SQLiteConnection(ConnectionString))
-            {
-                connection.Open();
-                var command = new SQLiteCommand("INSERT INTO Reviews (TeacherId, Text) VALUES (@teacherId, @text)", connection);
-                command.Parameters.AddWithValue("@teacherId", teacherId);
-                command.Parameters.AddWithValue("@text", text);
                 command.ExecuteNonQuery();
             }
         }
+        public void AddReview(int teacherId, string text)
+        {
+            using (var connection = new MySqlConnection(ConnectionString))
+            {
+                connection.Open();
+                var shortGuid = Guid.NewGuid().ToString().Substring(0, 18);
+                var command = new MySqlCommand("INSERT INTO review (id_review, rev_content, id_admin, id_auth, id_prof, rev_rating) VALUES (@idReview, @text, @idAdmin, @idAuth, @teacherId, @rating)", connection);
+                command.Parameters.AddWithValue("@idReview", shortGuid);
+                command.Parameters.AddWithValue("@text", text);
+                command.Parameters.AddWithValue("@idAdmin", 1); // Пример: ID администратора, заменить на фактическое значение
+                command.Parameters.AddWithValue("@idAuth", 1); // Пример: ID авторизованного пользователя, заменить на фактическое значение
+                command.Parameters.AddWithValue("@teacherId", teacherId);
+                command.Parameters.AddWithValue("@rating", 5); // Пример: рейтинг, заменить на фактическое значение
+                command.ExecuteNonQuery();
+            }
+        }
+        
     }
 
     public class Teacher
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Surname {get; set;}
+        public string Patronymic {get; set;}
+        public string PhoneNumber {get; set;}
+        public string Email {get; set;}
+        public string Image {get; set;}
+
+        public Teacher(string name, string surname, string patronymic, string phoneNumber, string email, string image)
+        {
+            Name = name;
+            Surname = surname;
+            Patronymic = patronymic;
+            PhoneNumber = phoneNumber;
+            Email = email;
+            Image = image;
+        }
     }
 
     public class Review
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public int TeacherId { get; set; }
         public string Text { get; set; }
-
-        public int Rate {get; set;}
     }
 }
